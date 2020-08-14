defea909680b745ee2a04f5c2c153e137439575d
Amelia Waggoner <wildginger@wavecable.com>
2020-08-14 16:26:10 -0700

    added folders from portfolio and additional files

--- server.js
+++ server.js
@@ -8,19 +8,22 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
-app.get('/hello', (request, response) => {
-  response.status(200).send('Hello');
-});
+// app.get('/hello', (request, response) => {
+//   response.status(200).send('Hello');
+// });
 
-app.get('/data', (request, response) => {
-  let airplanes = {
-    departure: Date.now(),
-    canFly: true,
-    pilot: 'Well Trained',
-  };
-  response.status(200).json(airplanes);
-});
+// app.get('/data', (request, response) => {
+//   let airplanes = {
+//     departure: Date.now(),
+//     canFly: true,
+//     pilot: 'Well Trained',
+//   };
+//   response.status(200).json(airplanes);
+// });
 
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+
+//Things we need don't need 11-22, don't need index.html file
+//Find portfolio drop and drag into public folder
